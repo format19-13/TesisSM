70efd4a9741a2cee7f74f34491ce61d69e79065e
vtortorella <veronica.tortorella@pyxis.com.uy>
2016-11-22 17:39:24 -0300

    Get edad from user id and user name

--- facebook_extractor/scrapingFacebook.py
+++ facebook_extractor/scrapingFacebook.py
@@ -4,40 +4,63 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support import ui
+from selenium.webdriver.common.keys import Keys
+import requests
 
-url1='https://www.facebook.com/profile.php?id=100014311837101&sk=about'
-url2='https://www.facebook.com/mcopes/about'
-url=url1
-# Opening the web browser
-driver = webdriver.PhantomJS()
-driver.get(url)
+def getEdad(user):
+    url='https://www.facebook.com'
+    urlInfoSection=url + '/'+user+ '/about?section=contact-info&pnref=about'
 
-fbUsername = "tesisvero@hotmail.com"
-fbPassword = "vero1234"
-emailFieldID = ".//*[@id='email']"
-passFieldID = ".//*[@id='pass']"
-loginButtonXPath = ".//input[@tabindex=4]"
-aboutButtonXPath = './/a[@data-tab-key="about"]'
+    # Opening the web browser
+    driver = webdriver.PhantomJS()
+    driver.get(url)
 
-flLogoXpath = "(//a[contains(@href, 'logo')])[1]"
-emailFieldElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(emailFieldID))
-passFieldElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(passFieldID))
-loginButtonElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(loginButtonXPath))
-emailFieldElement.click()
-emailFieldElement.clear()
-emailFieldElement.send_keys(fbUsername)
+    fbUsername = "tesisvero@hotmail.com"
+    fbPassword = "vero1234"
+    emailFieldID = ".//*[@id='email']"
+    passFieldID = ".//*[@id='pass']"
+    loginButtonXPath = ".//input[@tabindex=4]"
+    aboutButtonXPath = './/a[@data-tab-key="about"]'
 
-passFieldElement.click()
-passFieldElement.clear()
-passFieldElement.send_keys(fbPassword)
-loginButtonElement.click()
+    flLogoXpath = "(//a[contains(@href, 'logo')])[1]"
+    emailFieldElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(emailFieldID))
+    passFieldElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(passFieldID))
+    loginButtonElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(loginButtonXPath))
+    emailFieldElement.click()
+    emailFieldElement.clear()
+    emailFieldElement.send_keys(fbUsername)
 
-aboutButtonElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(aboutButtonXPath))
-aboutButtonElement.click()
+    passFieldElement.click()
+    passFieldElement.clear()
+    passFieldElement.send_keys(fbPassword)
+    loginButtonElement.click()
+    driver.get(urlInfoSection)
 
-fnac=driver.find_element_by_class_name("_c24").text
+    section=driver.find_elements_by_id("pagelet_basic")
+    salida=""
 
-if "Fecha de nacimiento" in fnac: 
-    print fnac.split('Fecha de nacimiento')[1]
+    for i in range(0, len(section)):
 
+    	aux= section[i].text
+        if "o de nacimiento" in aux:
+            salida=aux.split("o de nacimiento")[1][:5] 
+        else:
+            try:
+    	        sectionXPath = './/li[@data-privacy-fbid="8787510733"]'
+    	        elem=section[i].find_elements_by_xpath(sectionXPath)
+ 
+                for t in range(0, len(elem)):
+    	            elem2=elem[t].find_element_by_tag_name("div")
+    	            lstDivs= elem2.find_elements_by_tag_name("div")  
+                    #print driver.page_source
+    	            for x in range(0, len(lstDivs)):
+                        fnacText= lstDivs[x].text
+                        if "Fecha de nacimiento" in fnacText:
+                            salida=salida + lstDivs[x+1].text
+            except:
+                return "error"
+    return salida    
+#print driver.page_source
 
+print getEdad("mcopes")
+print getEdad("100014311837101")
